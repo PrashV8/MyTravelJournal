e8bc46d6f526c63be94e2ce53607b81385058353
PrashV8 <prashantvr8@gmail.com>
2024-03-09 01:16:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
-
 import './App.css';
 import Navbar from './Navbar';
 import dataset from "./data";
 import Card from "./Card"
 
-console.log(dataset)
+function chunkArray(arr, chunkSize) {
+    const chunkedArray = [];
+    for (let i = 0; i < arr.length; i += chunkSize) {
+        chunkedArray.push(arr.slice(i, i + chunkSize));
+    }
+    return chunkedArray;
+}
+
 function App() {
-const cards=dataset.map((data) =>{
-  return(
-    <Card
-    key={data.id}
-    {...data}
+    const chunkedCards = chunkArray(dataset, 3);
 
-    />
+    const rows = chunkedCards.map((row, rowIndex) => (
+        <div key={rowIndex} className="cards-list">
+            {row.map((data) => (
+                <Card key={data.id} {...data} />
+            ))}
+        </div>
+    ));
 
-    
-  )
-})
-
-
-  return (
-    <div className="App">
-      
-       
-        <Navbar/>
-    <section className="cards-list">
-      {cards}
-    </section>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar />
+            {rows}
+        </div>
+    );
 }
 
 export default App;
+
+
 
 
 
--- src/data.js
+++ src/data.js
@@ -9,19 +9,19 @@ googleMapsUrl: "https://maps.app.goo.gl/MESBDLgthz4hXVXHA",
 startDate: "12 Jan, 2023",
 endDate: "14 Jan, 2023", 
 description:"Haridwar is an ancient city and important Hindu pilgrimage site in North India's Uttarakhand state, where the River Ganges exits the Himalayan foothills. The largest of several sacred ghats (bathing steps), Har Ki Pauri hosts a nightly Ganga Aarti (river-worshipping ceremony) in which tiny flickering lamps are floated off the steps. Worshipers fill the city during major festivals including the annual Kanwar Mela. ",
-imageUrl : "https : / /unsplash . com/photos/WLxQvbMyfas"
+
 },
 
 {
   id: 2,
-  coverImg:"vaishnodevi.jpg",
-    title: "Vaishno Devi Mandir",
-  location: "Jummu and Kashmir,India",  
-googleMapsUrl: "https://maps.app.goo.gl/ts3JEWp8WbGGcb6UA",
+  coverImg:"leh.jpg",
+    title: "Khardung La Pass",
+  location: "Leh, India",  
+googleMapsUrl: "https://maps.app.goo.gl/3vudtu3R9sbxgUTQ9",
 startDate: "12 july, 2023",
 endDate: "18 july, 2023", 
-description:"Vaishno Devi is a manifestation of the Hindu mother goddess Durga or Adi Shakti. Vaishno Devi is worshipped as a combined avatar of the goddesses Mahakali, Mahalakshmi, and Mahasaraswati. And She is the Kaliyugi Avatar of Kalika. ",
-imageUrl : "https : / /unsplash . com/photos/WLxQvbMyfas"
+description:"This is the adventure capital of the country. Possessed with the top three mountain passes, which could be scaled on a vehicle, this region is famous for endless snow and dry adventure activities. Leh and the surrounding regions are also famous for unique culture, serene monasteries, incredible nature and best of all, hospitality. ",
+
 },
 
 {
@@ -33,11 +33,84 @@ googleMapsUrl: "https://maps.app.goo.gl/CaWoaqSHRVoMJSC77",
 startDate: "21 july, 2023",
 endDate: "30 july, 2023", 
 description:"Goa is a state in western India with coastlines stretching along the Arabian Sea. Its long history as a Portuguese colony prior to 1961 is evident in its preserved 17th-century churches and the area’s tropical spice plantations. Goa is also known for its beaches, ranging from popular stretches at Baga and Palolem to those in laid-back fishing villages such as Agonda. ",
-imageUrl : "https : / /unsplash . com/photos/WLxQvbMyfas"
+
 }
 ,
 
 
+{
+  id: 4,
+  coverImg:"anda.jpg",
+    title: "Andaman and Nicobar Islands" ,
+  location: "Andaman Islands, India",  
+googleMapsUrl: "https://maps.app.goo.gl/rGp8BEF4ZU7Lm638A",
+startDate: "12 Jan, 2023",
+endDate: "14 Jan, 2023", 
+description:"Although assumed as single destination, this place is a cluster of small islands, each famous for beaches and unique water activities. Starting from simple boating to scuba diving, you can enjoy all kinds of water activities here. On the silky beaches, the place provides endless entertainment and beautiful culture. ",
+
+},
+
+{
+  id: 5,
+  coverImg:"guju.jpg",
+    title: "A Scenic Fort City",
+  location: "Junagadh, India",  
+googleMapsUrl: "https://maps.app.goo.gl/ts3JEWp8WbGGcb6UA",
+startDate: "12 july, 2023",
+endDate: "18 july, 2023", 
+description:"Steeped in history, the city has seen several rulers during its existence from the Mauryas to the Mughals. As important as it is historically, Junagadh is also a popular pilgrimage destination for followers of several religions, such as Hinduism, Jainism, Islam, and Buddhism. Located in the foothills of Mount Girnar, the city is also close to one of the prominent national parks of India, the Gir National Park. ",
+
+},
+
+{
+  id: 6,
+  coverImg:"ris.jpg",
+    title: "Rishikesh" ,
+  location: "Rishikesh, India",  
+googleMapsUrl: "https://maps.app.goo.gl/KiPurjdyaiNRzwxv8",
+startDate: "21 july, 2023",
+endDate: "30 july, 2023", 
+description:"Yes, bordering River Ganges, this is yet another spiritual destination and best places to visit in India. However, it is just one of the faces of Rishikesh. It is also famous for yoga, meditation and Ayurvedic styled vacation, thanks to the Himalayan nature of the region. For youngsters, this is the place for beautiful adventure activities like rafting, zip lining, bungee jumping and much more. ",
+
+},
+
+
+{
+  id: 7,
+  coverImg:"tig.jpg",
+  title: " Jim Corbett National Park" ,
+  location: "Uttarakhand,India",  
+googleMapsUrl: "https://maps.app.goo.gl/GEmPT4B3en9XhWcC8",
+startDate: "21 july, 2023",
+endDate: "30 july, 2023", 
+description:"This is the top-most national park of the country. This national park is famous for numerous animals like tigers, bear, deer and others. You can also find a few endangered species in this park. A jeep safari through the park is the prime activity of the region. Jim Corbett National park is not at prime beauty throughout the year. Thus, choose your timing accordingly.",
+
+}
+,
+
+
+{
+  id: 8,
+  coverImg:"ooty.jpg",
+    title: " Ooty" ,
+  location: "Tamil Nadu, India",  
+googleMapsUrl: "https://maps.app.goo.gl/h1Decq4JqWPjM7gD9",
+startDate: "12 Jan, 2023",
+endDate: "14 Jan, 2023", 
+description:"It is a cliché hill station of Southern India. However, it is not called as ‘Queen of all hill stations’ for no reason. Ooty is famous for amazing destinations, view points, plantations, valleys and much more. On the other hand, Ooty is also famous for fresh vegetables, horse ride, boating, trekking, coracle ride and so on.",
+
+},
+
+{
+  id: 9,
+  coverImg:"ally.jpg",
+    title: " Alleppey",
+  location: "Kerala, India",  
+googleMapsUrl: "https://maps.app.goo.gl/mR8qnJ29NZ6QbnPA7",
+startDate: "12 july, 2023",
+endDate: "18 july, 2023", 
+description:"Kerala is called as the Venice of India. This is due to the factor that the land holds numerous backwaters, which are used as a prime mode of transportation. Those who visit Kerala should try the houseboat cruise at least once during the trip. Alleppey is the right place to be as close as possible to the backwaters, Ayurvedic centers and other traditional activities."
+}
 
 
 ]
